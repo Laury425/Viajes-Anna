3712b11ed2b0193bb6a46ba26e41f2b4e2e294b4
Laury425 <63926586+Laury425@users.noreply.github.com>
2020-04-18 20:24:42 -0600

    Update sendMail.php

--- contacto/sendMail.php
+++ contacto/sendMail.php
@@ -1,24 +1,21 @@
 <?php
-$nombre=$_POST['nombre'];
-$email=$_POST['email'];
-$telefono=$_POST['telefono'];
-$ciudad=$_POST['ciudad'];
-$celular=$_POST['celular'];
-$mensaje=$_POST['mensaje'];
+$nombre=$POST['nombre'];
+$email=$POST['email'];
+$telefono=$POST['telefono'];
+$ciudad=$POST['ciudad'];
+$celular=$POST['celular'];
+$mensaje=$POST['mensaje'];
 
 $cuerpo= "Ha recibido un Mensaje de: $nombre
-Datos enviados:
-Email: $email
+Datos Enviados:
+Email. $email
 Telefono: $telefono
 Celular: $celular
 Ciudad: $ciudad
 Mensajes: $mensaje";
 
 if(mail('laury.abril.cruz@gmail.com', $nombre, $cuerpo)){
-
-  echo "Su mensaje ha sido enviado";
-
+ echo "Su mensaje ha sido enviado";
 }else{
-  
-  echo"su mensaje no pudo ser enviado";
+echo"Su mensaje ha sido enviado";
 }
